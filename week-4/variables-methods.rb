9f818b2ec806b6f734daf80a78d18dd0c7fc8c5d
Eddie Bueno <eddiebueno93@gmail.com>
2016-01-20 00:59:08 -0800

    Edit Variables methods

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -15,3 +15,23 @@ puts "Hello #{first_name} #{middle_name} #{last_name}! How are you? "
 print "What is your favorite number? "
 fav_number = gets.chomp
 print "Now let's make it BIGGER and BETTER! ANNNNND KABAM!!! #{fav_number +1}. Now this is a bigger and better number"
+
+
+
+# How do you define a local variable?
+# You assign an object to a local variable. You do this by naming your local variable and writing = to an object. 
+
+# How do you define a method?
+# You define a method by saying def and the name of the method. You then assign the number of inputs. Then there is the body and then an output. You close it off with an end.
+
+# What is the difference between a local variable and a method?
+# A local variable only applies in the local workspace. A method can be used over and over again. 
+
+# How do you run a ruby program from the command line?
+# You can run a ruby program by typing ruby and then the file name.
+
+# How do you run an RSpec file from the command line?
+# You can runan rspen file from the command line by typin rspec and then the rspec file name.
+
+# What was confusing about this material? What made sense?
+# The material was pretty straight forward. Everything made sense there was no part that was difficult to understand.
