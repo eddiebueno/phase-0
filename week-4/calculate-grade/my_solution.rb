a1b5ed6622172679567970eea9ea0368edea979d
Eddie Bueno <eddiebueno93@gmail.com>
2016-01-24 19:13:40 -0800

    Add calculate-grade files

---
+++ week-4/calculate-grade/my_solution.rb
@@ -0,0 +1,25 @@
+# Calculate a Grade
+
+# I worked on this challenge [ with: John Pults ].
+
+
+
+# Your Solution Below
+
+def get_grade average
+  if average <= 100 && average > 89
+    return 'A'
+  end
+  if average <= 89 && average > 79
+    return 'B'
+  end
+  if average <= 79 && average > 69
+    return 'C'
+  end
+  if average <= 69 && average > 59
+    return 'D'
+  end
+  if average <= 59
+    return 'F'
+  end
+end
