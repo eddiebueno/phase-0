7899da1ff17e7a7481cadfccb9562108e934f471
Eddie Bueno <eddiebueno93@gmail.com>
2016-02-10 00:04:46 -0800

    Edit eloquent

--- week-7/eloquent.js
+++ week-7/eloquent.js
@@ -4,11 +4,29 @@
 
 // Program Structure
 // Write your own variable and do something to it.
+var number = 5;
+console.log(number + 1);
 
 
+
+var food = prompt("Please enter your favorite food", "pizza");
+
+    if (person != null){
+
+        console.log("That is my favorite food too!") ;
+    }
 
 
 // Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
+// Triangle
+string = "#"
+
+for (var i = 1; i <7; i+=1)
+
+   console.log(string)
+
+    string = string + "#";
+
 
 
 
@@ -16,7 +34,22 @@
 
 // Complete the `minimum` exercise.
 
+function min(int1,int2){
 
+    if ( int1 < int2)
+
+        return int1;
+
+    else
+
+        return int2;
+}
 
 // Data Structures: Objects and Arrays
 // Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+
+var me = {
+
+    name: "Ben", age: "Old", fav1: "Ice Cream", fav2: "pizza", fav3: "Cereal", quirk: "Umm" } 
+
+
